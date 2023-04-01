7490620ce4fdc68cb540097c725920fd531c1959
Radomanskyi <114738589+Radomanskyi@users.noreply.github.com>
2023-04-01 22:55:38 +0300

    3/10 commit

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -12,3 +12,9 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+const gallery = document.querySelector(`.gallery`);
+const imagesEl = images.map(({url, alt}) => 
+`<li"><img src="${url}"alt="${alt}" width = 200 height = 150 /></li>`).join(``);
+gallery.insertAdjacentHTML("afterbegin", imagesEl);
+gallery.classList.add(`js-gallery`);
+
