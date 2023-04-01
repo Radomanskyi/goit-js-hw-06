ef8dc49230a05d9c2de370ae043df239727b2dec
Radomanskyi <114738589+Radomanskyi@users.noreply.github.com>
2023-04-01 14:12:49 +0300

    2/10 commit

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.querySelector(`ul`);
+const elements = ingredients.map((ingredient) => {
+  const itemIngredients = document.createElement(`li`);
+  itemIngredients.classList.add(`item`);
+  itemIngredients.textContent = ingredient;
+  return itemIngredients;
+});
+
+ingredientsEl.append(...elements);
+console.log(elements);
