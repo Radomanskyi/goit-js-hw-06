e4dc5286de959f5e0ad532e3d3d38860661d05d6
Radomanskyi <114738589+Radomanskyi@users.noreply.github.com>
2023-04-03 11:25:33 +0300

    8/10 commit

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector(`form`);
+const input = document.querySelectorAll(`input`);
+const inputEmail = input[0];
+const inputPassword = input[1];
 
+
+form.addEventListener(`submit`, onFormSubmit);
+
+function onFormSubmit (event) {
+event.preventDefault();
+const {
+    elements: { email, password }
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+  console.log(`email: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}
