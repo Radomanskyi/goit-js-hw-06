44b2ddea6ebd472c69c614a92771a9f02ab6bd45
Radomanskyi <114738589+Radomanskyi@users.noreply.github.com>
2023-04-04 16:09:17 +0300

    6 DZ - JS

--- js/task-08.js
+++ js/task-08.js
@@ -15,6 +15,6 @@ function onFormSubmit (event) {
   if (email.value === "" || password.value === "") {
     return alert("Please fill in all the fields!");
   }
-  console.log(`{email: email.value, password: password.value}`);
+  console.log({email: email.value, password: password.value});
   event.currentTarget.reset();
 }
